5037fb0ac8dd8f1b0fd5844b98ad355aeb935144
Hasan9842321 <abeer.rocky@gmail.com>
2022-08-09 19:05:30 +0600

    Abir-mealinfo,request addes

---
+++ DAL/Repo/MealInfoRepo.cs
@@ -0,0 +1,45 @@
+﻿using DAL.EF;
+using DAL.Interfces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repo
+{
+    public class MealInfoRepo : IRepo<MealInfo, int, bool>
+    {
+
+        HMS_APIEntities db = new HMS_APIEntities();
+
+        public MealInfoRepo(HMS_APIEntities db)
+        {
+            this.db = db;
+        }
+        public bool Create(MealInfo obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<MealInfo> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public MealInfo Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(MealInfo obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DAL/Repo/Request_ServiceRepo.cs
@@ -0,0 +1,38 @@
+﻿using DAL.Interfces;
+using DAL.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repo
+{
+    public class Request_ServiceRepo : IRepo<Request_Services, int ,bool>
+    {
+        public bool Create(Request_Services obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Request_Services> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Request_Services Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Request_Services obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
