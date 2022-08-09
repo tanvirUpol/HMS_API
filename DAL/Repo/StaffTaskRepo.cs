bc762d4d04eee4d3d4cf4b18f83688b7448c389b
19-39570-1 <19-39570-1@student.aiub.edu>
2022-08-09 21:56:44 +0600

    StaffTask

---
+++ DAL/Repo/StaffTaskRepo.cs
@@ -0,0 +1,38 @@
+﻿using DAL.EF;
+using DAL.Interfces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repo
+{
+    public class StaffTaskRepo : IRepo<StaffTask, int, bool>
+    {
+        public bool Create(StaffTask obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<StaffTask> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public StaffTask Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(StaffTask obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
