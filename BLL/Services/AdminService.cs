a03c9c4fdbe32625c124017b5a930ba53cc66296
19-39570-1 <19-39570-1@student.aiub.edu>
2022-08-09 12:56:22 +0600

    Admin Table Done - Tanvir

--- BLL/Services/AdminService.cs
+++ BLL/Services/AdminService.cs
@@ -1,5 +1,6 @@
 ﻿using BLL.BOs;
 using DAL;
+using DAL.EF;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,9 +23,9 @@ namespace BLL.Services
                     Id = item.Id,
                     Name = item.Name,
                     Email = item.Email,
-                    //   Password = item.Password,
+                    Password = item.Password,
                     Phone = item.Phone,
-                    //  Type = item.Type,
+                    Type = item.Type,
 
 
                 });
@@ -32,5 +33,56 @@ namespace BLL.Services
             }
             return rdata;
         }
+
+        public static AdminModel Get(int id)
+        {
+            var data = DataAccessFactory.GetAdminDataAccess().Get(id);
+            var rdata = new AdminModel();
+            rdata.Id = data.Id;
+            rdata.Name = data.Name;
+            rdata.Email = data.Email;
+            rdata.Phone = data.Phone;
+            rdata.Type = data.Type;
+            rdata.Password = data.Password;
+      
+            return rdata;
+        }
+
+        public static bool Create(AdminModel item)
+        {
+            var admin = new Admin()
+            {
+                Email = item.Email,
+                Name = item.Name,
+                Password = item.Password,
+                Phone = item.Phone,
+                Type = 1
+                
+            };
+            return DataAccessFactory.GetAdminDataAccess().Create(admin);    
+        }
+
+        public static bool Update(AdminModel item)
+        {
+            var admin = new Admin()
+            {
+                Id = item.Id,
+                Email = item.Email,
+                Name = item.Name,
+                Password = item.Password,
+                Phone = item.Phone,
+                Type = 1
+
+            };
+            return DataAccessFactory.GetAdminDataAccess().Update(admin);
+        }
+
+        public static bool Delete(int id)
+        {
+            var data = DataAccessFactory.GetAdminDataAccess().Delete(id);
+            return data;
+        }
+
+
     }
 }
--- PresentationLayer/Controllers/AdminController.cs
+++ PresentationLayer/Controllers/AdminController.cs
@@ -1,4 +1,5 @@
-﻿using BLL.Services;
+﻿using BLL.BOs;
+using BLL.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,11 +11,46 @@ namespace PresentationLayer.Controllers
 {
     public class AdminController : ApiController
     {
-        [Route("api/student")]
+        [Route("api/admin/get")]
         [HttpGet]
         public HttpResponseMessage Get()
         {
             var data = AdminService.Get();
+            return Request.CreateResponse(HttpStatusCode.OK, data);
+
+        }
+
+
+        [Route("api/admin/get/{id}")]
+        [HttpGet]
+        public HttpResponseMessage Get(int id)
+        {
+            var data = AdminService.Get(id);
+            return Request.CreateResponse(HttpStatusCode.OK, data);
+
+        }
+
+        [Route("api/admin/create")]
+        [HttpPost]
+        public HttpResponseMessage Create(AdminModel n)
+        {
+            var data = AdminService.Create(n);
+            return Request.CreateResponse(HttpStatusCode.OK, data);
+        }
+
+        [Route("api/admin/update")]
+        [HttpPost]
+        public HttpResponseMessage Update(AdminModel n)
+        {
+            var data = AdminService.Update(n);
+            return Request.CreateResponse(HttpStatusCode.OK, data);
+        }
+
+        [Route("api/admin/delete/{id}")]
+        [HttpGet]
+        public HttpResponseMessage Delete(int id)
+        {
+            var data = AdminService.Delete(id);
             return Request.CreateResponse(HttpStatusCode.OK, data);
 
         }
