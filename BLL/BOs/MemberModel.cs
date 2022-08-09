9ab0bd22564fc817fac6ef8cfdcba8ed12404b81
Hasan9842321 <abeer.rocky@gmail.com>
2022-08-09 22:57:55 +0600

    Abir-Push Member model and services

---
+++ BLL/BOs/MemberModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.BOs
+{
+    public class MemberModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public int Age { get; set; }
+        public string Gname { get; set; }
+        public string Gender { get; set; }
+        public string Phone { get; set; }
+        public int Type { get; set; }
+        public int Room_Id { get; set; }
+    }
+}
---
+++ BLL/Services/MemberService.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using BLL.BOs;
+using DAL;
+using DAL.EF;
+using DAL.Repo;
+
+namespace BLL.Services
+{
+    public class MemberService
+    {
+        public static List<MemberModel> GetAllMember()
+        {
+            var config = new MapperConfiguration(c =>
+            c.CreateMap<Member, MemberModel>()
+            );
+
+            var mapper = new Mapper(config);
+            var data = mapper.Map<List<MemberModel>>(DataAccessFactory.GetMemberDataAccess().Get());
+
+
+            return data;
+        }
+
+        public static List<string> GetMember()
+        {
+            var data = DataAccessFactory.GetMemberDataAccess().Get().Select(e => e.Name).ToList();
+            return data;
+        }
+        public static void Add(MemberModel m)
+        {
+            var config = new MapperConfiguration(c =>
+            c.CreateMap<MemberModel, Member>()
+            );
+
+            var mapper = new Mapper(config);
+            var data = mapper.Map<Member>(m);
+
+        }
+
+        public static void Delete(MemberModel m)
+        {
+            var config = new MapperConfiguration(c =>
+            {
+                c.CreateMap<MemberModel, Member>();
+
+            });
+            var mapper = new Mapper(config);
+            var data = mapper.Map<Member>(m);
+            DataAccessFactory.GetMemberDataAccess().Delete(Convert.ToInt16(data));
+            
+        }
+
+        public static void Edit(MemberModel m)
+        {
+            var config = new MapperConfiguration(c =>
+            {
+                c.CreateMap<MemberModel, Member>();
+
+            });
+            var mapper = new Mapper(config);
+            var data = mapper.Map<Member>(m);
+            DataAccessFactory.GetMemberDataAccess().Update(data);
+        }
+    }
+}
