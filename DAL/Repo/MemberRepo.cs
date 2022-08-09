2d65aabf568d4a0e325731087943ced86ea64a5c
Hasan9842321 <abeer.rocky@gmail.com>
2022-08-09 14:02:02 +0600

    abir memberrepo Added

--- DAL/Repo/MemberRepo.cs
+++ DAL/Repo/MemberRepo.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DAL.EF;
+using DAL.Interfces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +8,38 @@ using System.Threading.Tasks;
 
 namespace DAL.Repo
 {
-    public class MemberRepo
+    public class MemberRepo : IRepo<Member, int, bool>
     {
 
+        HMS_APIEntities db = new HMS_APIEntities();
+
+        public MemberRepo(HMS_APIEntities db)
+        {
+            this.db = db;
+        }
+        public bool Create(Member obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Member> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Member Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Member obj)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
