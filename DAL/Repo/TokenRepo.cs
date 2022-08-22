9dea108058ada355aad0b60491870104e78ad7d0
19-39570-1 <19-39570-1@student.aiub.edu>
2022-08-23 05:00:04 +0600

    Auth Done

---
+++ DAL/Repo/TokenRepo.cs
@@ -0,0 +1,53 @@
+﻿using DAL.EF;
+using DAL.Interfces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repo
+{
+    public class TokenRepo : IRepo<Token, string, Token>
+    {
+        HMS_APIEntities db;
+        public TokenRepo(HMS_APIEntities db)
+        {
+            this.db = db;
+        }
+        public Token Create(Token obj)
+        {
+            db.Tokens.Add(obj);
+            db.SaveChanges();
+            return obj;
+        }
+        public bool Delete(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Token> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Token Get(string token)
+        {
+            return db.Tokens.FirstOrDefault(t => t.Token1.Equals(token));
+        }
+
+
+
+        public bool Update(Token obj)
+        {
+            var exst = db.Tokens.FirstOrDefault(x => x.Token1.Equals(obj.Token1));
+            if (exst != null)
+            {
+                db.Entry(exst).CurrentValues.SetValues(obj);
+                db.SaveChanges();
+                return true;
+            }
+            return false;
+        }
+    }
+}
